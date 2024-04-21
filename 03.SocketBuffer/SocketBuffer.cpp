52b6c4abfcac781ebaaaf7b8754e28bd6f69ecc9
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-04-21 23:49:55 +0900

    Section 2. SocketBuffer

---
+++ 03.SocketBuffer/SocketBuffer.cpp
@@ -0,0 +1,43 @@
+// SocketBuffer.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32")
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//윈속 초기화
+	WSADATA wsa = { 0 };
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		puts("ERROR: 윈속을 초기화 할 수 없습니다.");
+		return 0;
+	}
+
+	//소켓 생성
+	SOCKET hSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+	{
+		puts("ERROR: 소켓을 생성할 수 없습니다.");
+		return 0;
+	}
+
+	//소켓의 '송신' 버퍼의 크기를 확인하고 출력한다.
+	int nBufSize = 0, nLen = sizeof(nBufSize);
+	if (::getsockopt(hSocket, SOL_SOCKET,
+		SO_SNDBUF, (char*)&nBufSize, &nLen) != SOCKET_ERROR)
+		printf("Send buffer size: %d\n", nBufSize);
+
+	//소켓의 '수신' 버퍼의 크기를 확인하고 출력한다.
+	nBufSize = 0;
+	nLen = sizeof(nBufSize);
+	if (::getsockopt(hSocket, SOL_SOCKET,
+		SO_RCVBUF, (char*)&nBufSize, &nLen) != SOCKET_ERROR)
+		printf("Receive buffer size: %d\n", nBufSize);
+
+	//소켓을 닫고 종료.
+	::closesocket(hSocket);
+	::WSACleanup();
+	return 0;
+}
